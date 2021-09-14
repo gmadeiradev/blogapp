c9cd148243cfe0da2f65081cf9eab156fc2f0358
Madeira <gabrielsmadeira@fac.pe.senac.br>
2021-09-14 07:26:05 -0300

    Setting: middleware/ session/ flash message

--- app.js
+++ app.js
@@ -4,10 +4,28 @@ const bodyParser = require("body-parser");
 const app = express();
 const admin = require("./routes/admin");
 const path = require("path");
-const { Mongoose } = require("mongoose");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 // * configs
+//session
+app.use(session({
+    secret: "nodeCourse",
+    resave: true,
+    saveUninitialized: true
+}));
+
+// flash
+app.use(flash());
+
+// middleware
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash("success_msg");
+    res.locals.error_msg = req.flash("error_msg");
+    next();
+});
+
 // bodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -15,16 +33,23 @@ app.use(bodyParser.json());
 //hablebars
 app.engine("handlebars", handlebars({ defaultLayout: "main" }))
 app.set("view engine", "handlebars");
+
 // mongoose
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/blogapp").then(() => {
     console.log("MongoDB connected :D");
 }).catch((err) => {
-    console.log("Error trying to connect to MongoDB :( "+err);
+    console.log("Error trying to connect to MongoDB :( " + err);
 });
 
 // * public
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
+
+//middleware
+app.use((req, res, next) => {
+    console.log("Hi, I'm middleware!");
+    next();
+});
 
 // * routes
 app.use("/admin", admin);
