79ff6ed078436f0e7080f2e89ab32c96abbd2082
Madeira <gabrielsmadeira@fac.pe.senac.br>
2021-09-16 11:13:43 -0300

    Edit categories

--- routes/admin.js
+++ routes/admin.js
@@ -7,36 +7,33 @@ const Category = mongoose.model("categories");
 router.get("/", (req, res) => {
     res.render("admin/index");
 });
-
+// posts page
 router.get("/posts", (req, res) => {
     res.send("Posts page");
 });
-
+ // categories list
 router.get("/categories", (req, res) => {
-    // categories list
     Category.find().lean().sort({ date: "desc" }).then((categories) => {
         res.render("admin/categories", { categories: categories });
     }).catch((err) => {
-        req.flash("error_msg", "Error trying to list categories");
+        req.flash("error_msg", "Error trying to list categories!");
+        console.log(err);
         res.redirect("/admin");
     });
 });
-
+// new category
 router.post("/categories/new", (req, res) => {
     let error = [];
 
     if (!req.body.name || typeof req.body.name == undefined || req.body.name == null) {
         error.push({ text: "Invalid Name!" });
     }
-
     if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
-        error.push({ text: "Invalid Slug" });
+        error.push({ text: "Invalid Slug!" });
     }
-
     if (req.body.name.length < 2) {
         error.push({ text: "Category Name too small!" });
     }
-
     if (error.length > 0) {
         res.render("admin/addcategories", { error: error });
     } else {
@@ -48,10 +45,42 @@ router.post("/categories/new", (req, res) => {
             req.flash("success_msg", "Successfully created category!");
             res.redirect("/admin/categories");
         }).catch((err) => {
-            req.flash("error_msg", "Error trying to create category! Please try again.");
+            req.flash("error_msg", "Error trying to create category! Please try again!");
+            console.log(err);
             res.redirect("/admin");
         });
     }
+});
+
+router.get("/categories/edit/:id", (req, res) => {
+    Category.findOne({ _id: req.params.id }).lean().then((category) => {
+        res.render("admin/editcategories", { category: category });
+    }).catch((err) => {
+        req.flash("error_msg", "This category doesn't exists! ");
+        console.log(err);
+        res.redirect("/admin/categories");
+    });
+});
+
+router.post("/categories/edit", (req, res) => {
+    Category.findOne({ _id: req.body.id }).then((category) => {
+        //
+        category.name = req.body.name;
+        category.slug = req.body.slug;
+
+        category.save().then(() => {
+            req.flash("success_msg", "Successfully category edit!");
+            res.redirect("/admin/categories");
+        }).catch((err) => {
+            req.flash("error_msg", "Error trying to edit category");
+            console.log(err);
+            res.redirect("/admin/categories");
+        })
+    }).catch((err) => {
+        req.flash("error_msg", "There was an error editing the category!");
+        console.log(err);
+        res.redirect("/admin/categories");
+    })
 });
 
 router.get("/categories/add", (req, res) => {
